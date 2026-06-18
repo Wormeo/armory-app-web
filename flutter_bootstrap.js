833c10f6332044a6399a0b4f9a312fd2494efde0
Wormeo <Wormeo@users.noreply.github.com>
2026-06-18 17:31:21 +0000

    deploy: 5ee8d809a23a05f5221790f14a045498c67c003b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "708998569" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4009451136" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
